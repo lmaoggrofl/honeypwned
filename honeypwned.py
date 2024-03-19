29c8c958b80ed71cb1315d44fa975363c2197515
lmaoggrofl <110363544+lmaoggrofl@users.noreply.github.com>
2024-03-19 11:18:21 +0800

    Update honeypwned.py
    added ssh to honeypot

--- honeypwned.py
+++ honeypwned.py
@@ -5,18 +5,26 @@ import sys
 from socket import socket, timeout
 from flask import Flask, render_template, send_from_directory
 import os
+import paramiko
+import datetime
+
+# Global Variable
+BIND_IP = '0.0.0.0'
+config_filepath = 'honeypwned.ini'
+ssh_port = 22  # Default SSH port
 
 def handle_client(client_socket, port, ip, remote_port):
-    logger.info("Connection Received on port: %s from %s:%d " % (port, ip, remote_port))
+    current_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%f")[:-3]
+    logger.info("[%s] Connection Received on port: %s from %s:%d " % (current_time, port, ip, remote_port))
     client_socket.settimeout(4)
     try:
         data = client_socket.recv(64)
-        logger.info("Data received: %s from %s:%d - %s" % (port, ip, remote_port, data))
+        logger.info("[%s] Data received on port: %s from %s:%d - %s" % (current_time, port, ip, remote_port, data))
         client_socket.send("Access Denied.\n".encode('utf8'))
     except timeout:
         pass
     except ConnectionResetError as e:
-        logger.error("Connection Reset Error occurred")
+        logger.error("[%s] Connection Reset Error occurred" % current_time)
     client_socket.close()
 
 def start_new_listener_thread(port):
@@ -90,8 +98,46 @@ def start_flask_server():
 
     app.run(host='0.0.0.0', port=80)
 
-BIND_IP = '0.0.0.0'
-config_filepath = 'honeypwned.ini'
+def generate_or_read_ssh_key():
+    key_folder = 'Files'
+    key_path = os.path.join(key_folder, 'ssh_key')
+    if not os.path.exists(key_path):
+        # Generate SSH key if not exists
+        key = paramiko.RSAKey.generate(2048)
+        key.write_private_key_file(key_path)
+    else:
+        # Read SSH key
+        key = paramiko.RSAKey(filename=key_path)
+    return key
+
+class SSHServer(paramiko.ServerInterface):
+    def check_auth_password(self, username: str, password: str) -> int:
+        current_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%f")[:-3]
+        logger.info('[%s] SSH login attempts - %s %s' % (current_time, username, password))
+        return paramiko.AUTH_FAILED
+
+def start_ssh_server():
+    global ssh_port
+    ssh_key = generate_or_read_ssh_key()
+
+    server_socket = socket()
+    server_socket.bind((BIND_IP, ssh_port))
+    server_socket.listen(5)
+
+    while True:
+        client, addr = server_socket.accept()
+        current_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%f")[:-3]
+        logger.info('[%s] Connection Received on port: 22 from: %s:%d' % (current_time, addr[0], addr[1]))
+        
+        try:
+            transport = paramiko.Transport(client)
+            transport.add_server_key(ssh_key)
+            server = SSHServer()
+            transport.start_server(server=server)
+        except Exception as e:
+            pass
+
+logging.getLogger("paramiko").setLevel(logging.CRITICAL)
 
 config = configparser.ConfigParser()
 config.read(config_filepath)
@@ -118,6 +164,12 @@ except Exception as e:
 if len(ports) < 1:
     print('[!] No ports provided.')
     sys.exit(1)
+
+# Start SSH server if SSH port is configured
+if '22' in ports_list:
+    ports_list.remove('22')
+    ssh_thread = threading.Thread(target=start_ssh_server)
+    ssh_thread.start()
 
 # Check if port 80 is in the ports list
 if '80' in ports_list:
